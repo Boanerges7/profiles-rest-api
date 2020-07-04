f5e1427ca84b72d7a331279a74b774c61b6a94e2
Abbe <abjacklyn@gmail.com>
2020-07-04 12:32:22 +0000

    Added user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters # for filter feature
@@ -124,3 +126,20 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
 	"""Handle creating user authentication tokens"""
 	renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	"""Handles creating, reading and updating profile feed items"""
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeedItemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+	permission_classes = (
+		permissions.UpdateOwnStatus,
+		IsAuthenticatedOrReadOnly
+		# To not allow non authenticated users, just use IsAuthenticated
+	)
+
+	# Overrinding serializer POST method in this viewset
+	def perform_create(self, serializer):
+		"""Sets the user profile to the logged in user"""
+		serializer.save(user_profile=self.request.user)
